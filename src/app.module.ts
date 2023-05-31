eb9076f62fb476415762b3bae8566cd201ab0568
narekpetrosyan <narek.petrosyan988@gmail.com>
2023-05-31 14:30:21 +0400

    added env variables validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,26 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AtGuard } from './auth/guards/at.guard';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MYSQL_HOST: Joi.string().default('localhost'),
+        MYSQL_PORT: Joi.number().default(3306),
+        MYSQL_USER: Joi.string().default('root'),
+        MYSQL_ROOT_PASSWORD: Joi.string().default('root'),
+        MYSQL_DATABASE: Joi.string().required(),
+        JWT_ACCESS_SECRET: Joi.string().required(),
+        JWT_REFRESH_SECRET: Joi.string().required(),
+      }),
+    }),
     DatabaseModule,
     AuthModule,
     UserModule,
   ],
-  controllers: [],
   providers: [
     {
       provide: APP_GUARD,
