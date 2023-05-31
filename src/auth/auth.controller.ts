982d0a3f56d17f15fba59b79e99b1ae1a5ace146
narekpetrosyan <narek.petrosyan988@gmail.com>
2023-05-31 18:04:11 +0400

    added swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,28 +5,49 @@ import { GetCurrentUserId } from 'src/lib/decorators/get-user-id.decorator';
 import { Public } from 'src/lib/decorators/public.decorator';
 import { RtGuard } from './guards/rt.guard';
 import { GetCurrentUser } from 'src/lib/decorators/get-user.decorator';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { CreateUserDto } from 'src/user/dto/createUser.dto';
 
+@ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Register a user' })
+  @ApiResponse({
+    description: 'Returns access and refresh tokens',
+  })
   @Public()
   @Post('register')
-  register(@Body() createUserDto: Record<string, any>) {
+  register(@Body() createUserDto: CreateUserDto) {
     return this.authService.register(createUserDto);
   }
 
+  @ApiOperation({ summary: 'Login' })
+  @ApiResponse({
+    description: 'Returns access and refresh tokens',
+  })
   @Public()
   @Post('login')
   login(@Body() data: AuthDto) {
     return this.authService.login(data);
   }
 
+  @ApiOperation({ summary: 'Logout' })
   @Get('logout')
   logout(@GetCurrentUserId() userId: number) {
     this.authService.logout(userId);
   }
 
+  @ApiOperation({ summary: 'Refresh access token' })
+  @ApiResponse({
+    description: 'Returns refreshed access token and refresh token',
+  })
   @Public()
   @UseGuards(RtGuard)
   @Post('/refresh')
@@ -37,6 +58,7 @@ export class AuthController {
     return await this.authService.refreshTokens(userId, refreshToken);
   }
 
+  @ApiBearerAuth('jwt-auth')
   @Get('profile')
   profile(@Req() req) {
     return req.user;
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Authentication demo')
+    .setDescription('A simple authentication demo')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'jwt-auth',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/documentation', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
